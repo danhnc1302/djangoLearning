7dbc9ab8d88cbc7270d5ab86e798d732f9f92005
danhnc <danhnc1302@gmail.com>
2024-01-14 15:35:51 +0700

    first commit

---
+++ userAPI/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# Create your views here.
+
+class TestView(APIView):
+    def get(self, request, format=None):
+        print("API Was Called")
+        return Response("You Made It", status=200)
